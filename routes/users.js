3249a2790b9a5121b46d71c17bdcf5b298c4f922
memo figueredo <memof80@gmail.com>
2020-11-19 16:45:59 -0500

    passport

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,8 @@ var express = require('express');
 const bodyParser = require('body-parser');
 
 var User = require('../models/user');
+var passport = require('passport');
+
 var router = express.Router();
 
 router.use(bodyParser.json());
@@ -12,74 +14,28 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/signup', function(req, res, next){
-  User.findOne({username: req.body.username})
-  .then((user) => {
-    if (user != null) {
-      var err = new Error('User' + req.body.username + ' alredy exists!');
-      err.status = 403;
-      next(err);
+  User.register(new User({username: req.body.username}), 
+  req.body.password, (err, user) => {
+    if (err) {
+      res.statusCode = 500;
+      res.setHeader('Content-Type', 'application/json');
+      res.json({err: err});
     }
     else {
-      return User.create({
-        username: req.body.username, 
-        password: req.body.password});
+      passport.authenticate('local')(req,res, () => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({success: true, status: 'Registration Sucessful!'});
+      });
     }
-  })
-  .then((user) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.json({status: 'Registration Sucessful!', user: user});
-  },(err) => next(err))
-  .catch((err) => next(err));
+  });
 });
 
-router.post('/login', function(req, res, next){
+router.post('/login', passport.authenticate('local'), (req, res) => {
 
-  if (!req.session.user) {
-
-    var authHeader = req.headers.authorization;
-
-    if (!authHeader) {
-      var err = new Error('You are not authentication!');
-  
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      return next(err);
-    }
-  
-    var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-  
-    var username = auth[0];
-    var password = auth[1];
-  
-    User.findOne({username: username})
-    .then((user) => {
-      if (user === null) {
-        var err = new Error('User' + username + 'does not exists');
-        err.status = 401;
-        return next(err);
-      } 
-      else if (user.password !== password) {
-        var err = new Error('Your password is incorrect');
-        err.status = 401;
-        return next(err);
-      }
-      else if (user.username === username && user.password === password) {
-      req.session.user = 'authenticated';
-      res.statusCode = 200;
-      res.setHeader('Content-Type', 'text/plain');
-      res.end('You are authenticated');
-      }
-    })
-    .catch((err) => next(err));
-
-  } 
-  //usuario ya ha registrado session
-  else {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('You are alredy authenticated');
-  }
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.json({success: true, status: 'You are Successfully logeed in!'});
 
 });
 
