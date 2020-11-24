2b9dd39eb4142e85492ef66bbb7b39c29d09022e
memo figueredo <memof80@gmail.com>
2020-11-24 18:06:39 -0500

    file upload

--- app.js
+++ app.js
@@ -18,6 +18,8 @@ var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var promosRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
+//uploading files
+var uploadRouter = require('./routes/uploadRouter');
 
 const mongoose = require('mongoose');
 
@@ -78,6 +80,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes', dishRouter);
 app.use('/promotions', promosRouter);
 app.use('/leaders', leaderRouter);
+
+//upload files 
+app.use('/imageUpload', uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
