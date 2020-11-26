5894ebd2ba6fc5b6dd790d22be7a17f9c9234a49
memo figueredo <memof80@gmail.com>
2020-11-25 20:38:01 -0500

    necesario mirar configuracion ya que no guarda en la base de datos

--- config.js
+++ config.js
@@ -1,4 +1,9 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '186994479746079',
+        clientSecret: 'f3718e562074eb2e5472e4583759fccc',
+        fbfbGraphVersion: 'v9.0'
+    }
 }
